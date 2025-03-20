5e32952c7bd425afd934dcb368a0b1a607dd2b44
AhmadAlkaseb <ahmadalkasebpri@icloud.com>
2025-03-20 14:17:37 +0100

    Upload of all DAOS

---
+++ Backend/src/main/java/daos/IDAO.java
@@ -0,0 +1,11 @@
+package daos;
+
+import java.util.List;
+
+public interface IDAO<T> {
+    List<T> getAll();
+    T getById(int id);
+    T create(T entity);
+    T update(T entity);
+    T delete(int id);
+}
